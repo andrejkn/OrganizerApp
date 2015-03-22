7c872141b113b91437bbff79208172942f5f0a43
andrej <andrej@eidosearch.com>
2015-03-21 20:10:44 -0400

    packages

---
+++ server/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by andrej on 21/03/15.
+ */
+var port = Number(process.env.PORT || 8000);
+var express = require('express');
+var app = express();
+
+var appDir = __dirname + '/../client';
+
+console.log(appDir);
+
+app.use(express.static(appDir));
+var server = app.listen(port, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
